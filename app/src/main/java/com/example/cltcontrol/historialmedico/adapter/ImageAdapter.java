57daa438d839cb572d06734fe337a0767df4d063
Unknown <jorge_18link@hotmail.com>
2018-08-04 23:02:35 -0500

    Pasando imagen a base64 para subirla al servidor

--- app/src/main/java/com/example/cltcontrol/historialmedico/adapter/ImageAdapter.java
+++ app/src/main/java/com/example/cltcontrol/historialmedico/adapter/ImageAdapter.java
@@ -1,23 +1,40 @@
 package com.example.cltcontrol.historialmedico.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
-import com.example.cltcontrol.historialmedico.R;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImageAdapter extends BaseAdapter {
+
+    File imgFile;
+    private List<Bitmap> misFotosBitmap;
+
     private Context mContext;
 
-    public ImageAdapter(Context c){
-        mContext = c;
+    public ImageAdapter(Context context, List<String> rutas){
+        mContext = context;
+        misFotosBitmap = new ArrayList<>();
+        for (String actual : rutas) {
+            imgFile = new File(actual);
+
+            if(imgFile.exists()) {
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                misFotosBitmap.add(myBitmap);
+            }
+        }
     }
 
     public int getCount(){
-        return mThumbsIds.length;
+        return misFotosBitmap.size();
     }
 
     public Object getItem(int position){
@@ -30,30 +47,19 @@ public class ImageAdapter extends BaseAdapter {
 
     public View getView(int position, View convertView, ViewGroup parent){
         ImageView imageView = new ImageView(mContext);
-        imageView.setLayoutParams(new GridView.LayoutParams(300,300));
+        imageView.setLayoutParams(new GridView.LayoutParams(250,250));
         imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         imageView.setPadding(8,8,8,8);
-        imageView.setImageResource(mThumbsIds[position]);
+
+        imageView.setImageBitmap(misFotosBitmap.get(position));
         return imageView;
     }
 
-    private Integer[] mThumbsIds = {
-            R.drawable.perrito1,
-            R.drawable.perrito2,
-            R.drawable.perrito1,
-            R.drawable.perrito2,
-            R.drawable.perrito1,
-            R.drawable.perrito2,
-            R.drawable.perrito1,
-            R.drawable.perrito2,
-            R.drawable.perrito1,
-            R.drawable.perrito2,
-            R.drawable.perrito1,
-            R.drawable.perrito2,
-            R.drawable.perrito1,
-            R.drawable.perrito2,
-            R.drawable.perrito1,
-            R.drawable.perrito2
-    };
+    public List<Bitmap> getMisFotosBitmap() {
+        return misFotosBitmap;
+    }
 
+    public void setMisFotosBitmap(List<Bitmap> misFotosBitmap) {
+        this.misFotosBitmap = misFotosBitmap;
+    }
 }
